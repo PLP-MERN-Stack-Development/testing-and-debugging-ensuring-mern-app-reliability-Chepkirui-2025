b675c1e755d81d308add35318f0f1d21a62f734d
Chepkirui-2025 <franciscachepkirui@gmail.com>
2025-12-03 15:42:09 +0300

    add jest config to client

---
+++ client/src/tests/unit/jest.config.js
@@ -0,0 +1,29 @@
+// jest.config.js - Jest configuration for client-side testing
+
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/src/tests/__mocks__/fileMock.js',
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/index.js',
+    '!src/reportWebVitals.js',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+  testMatch: [
+    '<rootDir>/src/tests/**/*.test.{js,jsx}',
+  ],
+};
